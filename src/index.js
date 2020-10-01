5361f5e36f3eca042447ad6d48ae4a0ea82a166e
Nina Sitaeva <sitaeva.nn@gmail.com>
2020-10-01 23:21:25 +0300

    init: start working on task typical array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || !array.length) {
+      return 0;
+    }
+    else {
+      array.sort((a, b) => a - b);
+      return array[0];
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+  else {
+    array.sort((a, b) => a - b);
+    return array[array.length-1];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || !array.length) {
+    return 0;
+  }
+  else {
+    let result = array.reduce((sum, current) => sum + current, 0);
+    return result = result / array.length;
+  }
 }
